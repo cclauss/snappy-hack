fc62a20c730c217b39c892d28ca67c49c5146198
cclauss <cclauss@bluewin.ch>
2018-03-20 22:02:34 +0100

    Create snappy-hack.py

---
+++ snappy-hack.py
@@ -0,0 +1,20 @@
+import snappy
+
+def out(x):
+    print(type(x), len(x), x)
+
+s = 'The quick brown fox jumped over the lazy dog.'
+out(s)
+compressed = snappy.compress(s)
+out(compressed)
+try:
+    buf = buffer(compressed)
+    out(buf)
+    buf_d = snappy.decompress(buf)
+    out(buf_d)
+except NameError:
+    print('buffer was removed from Python 3.')
+mem = memoryview(compressed)
+out(mem)
+mem_d = snappy.decompress(mem)
+out(mem_d)
